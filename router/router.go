d971b5ca879b0d119ba9d8c4e0ce2691f238012c
Théophile Roos <slashformotion@protonmail.com>
2022-09-16 16:06:09 +0200

    Update router

--- router/router.go
+++ router/router.go
@@ -2,14 +2,24 @@ package router
 
 import (
 	"github.com/ditrit/badaas/controllers"
+	"github.com/ditrit/badaas/router/middlewares"
 	"github.com/gorilla/mux"
 )
 
 // Default router of badaas, initialize all routes.
 func SetupRouter() *mux.Router {
 	router := mux.NewRouter()
+	router.Use(middlewares.LoggerMW)
 
 	router.HandleFunc("/info", controllers.Info).Methods("GET")
+
+	// basic auth login
+	router.HandleFunc("/login", controllers.BasicLoginHandler).Methods("POST")
+
+	protectedRouter := router.PathPrefix("/").Subrouter()
+	protectedRouter.Use(middlewares.AuthenticationMW)
+	// JWT refresh
+	protectedRouter.HandleFunc("/refresh", controllers.RefreshJWT).Methods("GET")
 
 	return router
 }
