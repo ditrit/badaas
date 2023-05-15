614610d6e16432e9c8d61a9c5363f775cccba6ea
Franco Liberali <franco.liberali@gmail.com>
2023-05-15 11:54:07 +0200

    filter by null not supported yet

--- badorm/crudRepository.go
+++ badorm/crudRepository.go
@@ -278,7 +278,7 @@ func divideConditionsByEntity(
 
 	for attributeName, expectedValue := range conditions {
 		switch typedExpectedValue := expectedValue.(type) {
-		case float64, bool, string, nil:
+		case float64, bool, string:
 			thisEntityConditions[attributeName] = expectedValue
 		case map[string]any:
 			joinConditions[attributeName] = typedExpectedValue
