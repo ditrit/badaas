b765da4b22a3fdd2c7a09c60383cf876efc79e3f
Franco Liberali <franco.liberali@gmail.com>
2023-05-30 15:54:02 +0200

    export badorm automigrate

--- badorm/autoMigrate.go
+++ badorm/autoMigrate.go
@@ -1,29 +1,21 @@
 package badorm
 
 import (
-	"reflect"
-
 	"github.com/elliotchance/pie/v2"
-	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
-func autoMigrate(modelsLists [][]any, db *gorm.DB, logger *zap.Logger) error {
+func AutoMigrate(models []any, db *gorm.DB) error {
+	return autoMigrate([][]any{models}, db)
+}
+
+func autoMigrate(modelsLists [][]any, db *gorm.DB) error {
 	allModels := pie.Flat(modelsLists)
-	registeredModels := pie.Map(allModels, func(model any) string {
-		return reflect.TypeOf(model).String()
-	})
-	logger.Sugar().Debug(
-		"Registered models: ",
-		registeredModels,
-	)
 
 	err := db.AutoMigrate(allModels...)
 	if err != nil {
-		logger.Error("migration failed")
 		return err
 	}
 
-	logger.Info("AutoMigration was executed successfully")
 	return nil
 }
